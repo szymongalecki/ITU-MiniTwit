0b207f7fcbf4ae645e721a4c8c0831f687dbfad1
Marcus Gunnebo <marcusgunnebo@marcuss-macbook-air.home>
2023-02-12 14:24:46 +0100

    updated settings and urls

--- ITU_MiniTwit/MiniTwit/admin.py
+++ ITU_MiniTwit/MiniTwit/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Follower, User, Message
 
-# Register your models here.
+admin.site.register(Follower)
+admin.site.register(User)
+admin.site.register(Message)
