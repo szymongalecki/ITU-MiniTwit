6fd89eeb3a83fb3bfb1297132e6293a0539e684e
Marcus Gunnebo <marcusgunnebo@marcuss-macbook-air.home>
2023-02-12 14:25:29 +0100

    first view

--- ITU_MiniTwit/MiniTwit/views.py
+++ ITU_MiniTwit/MiniTwit/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Message, User
 
-# Create your views here.
+def timeline(request):
+    messages = Message.objects.filter()
+    user = User.objects.get(id=request.user.id)
+    context = {
+        "messages":messages,
+        "user":user
+    }
+    return render(request, 'MiniTwit/timeline.html', context)
