826278d2be14cc75a13a1f93451d06427ea71014
Marcus Gunnebo <marcusgunnebo@Marcuss-MacBook-Air.local>
2023-02-14 16:47:40 +0100

    refactoring marcus

--- ITU_MiniTwit/MiniTwit/urls.py
+++ ITU_MiniTwit/MiniTwit/urls.py
@@ -5,4 +5,10 @@ app_name = 'crm'
 
 urlpatterns = [
     path('', timeline, name="timeline"),
+    path('login/', login, name="login"),
+    path('logout/', logout, name="logout"),
+    path('addmessage/', add_message, name="add-message"),
+    path('<str:pk>/', user_timeline, name="user-timeline"),
+    path('<str:pk>/follow', follow_user, name="follow-user"),
+    path('<str:pk>/unfollow', unfollow_user, name="unfollow-user"),
 ]
--- ITU_MiniTwit/MiniTwit/views.py
+++ ITU_MiniTwit/MiniTwit/views.py
@@ -6,11 +6,50 @@ def timeline(request):
         user = User.objects.get(id=request.user.id)
         follower = Follower.objects.filter(who_id = user.id).values_list('whom_id')
         messages = Message.objects.filter(author_id__in = follower)
+        view = "user_timeline"
     else:
         user = None
-        messages = Message.objects.get()
+        messages = Message.objects.all()
+        view = "public_timeline"
     context = {
+        "view":view,
         "messages":messages,
         "user":user
     }
     return render(request, 'MiniTwit/timeline.html', context)
+
+def user_timeline(request, pk):
+    profile_user = User.objects.get(username=pk)
+    messages = Message.objects.filter(author_id = profile_user.id)
+    if request.user.is_authenticated:
+        user = User.objects.get(id=request.user.id)
+        if Follower.objects.filter(who_id = user.id).filter(whom_id = profile_user.id).exists():
+            followed = True
+        else:
+            followed = False
+    else:
+        user = None
+    context = {
+        "profile_user":profile_user,
+        "view":"profile_user_timeline",
+        "messages":messages,
+        "user":user,
+        "followed":followed
+    } 
+    print(context)
+    return render(request, 'MiniTwit/timeline.html', context)
+
+def login(request):
+    return render(request, 'MiniTwit/login.html', {})
+
+def logout(request):
+    return render(request, 'MiniTwit/login.html', {})
+
+def follow_user(request):
+    return render(request, 'MiniTwit/login.html', {})
+
+def unfollow_user(request):
+    return render(request, 'MiniTwit/login.html', {})
+
+def add_message(request):
+    return render(request, 'MiniTwit/login.html', {})
