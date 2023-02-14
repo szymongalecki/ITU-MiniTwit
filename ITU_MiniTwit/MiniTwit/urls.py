26a139228c9a28604f09d39de96587ed610f76ce
Marcus Gunnebo <marcusgunnebo@marcuss-macbook-air.home>
2023-02-14 22:09:59 +0100

    timeline rewrite

--- ITU_MiniTwit/MiniTwit/urls.py
+++ ITU_MiniTwit/MiniTwit/urls.py
@@ -5,10 +5,11 @@ app_name = 'crm'
 
 urlpatterns = [
     path('', timeline, name="timeline"),
+    path('public/', public_timeline, name="public-timeline"),
     path('login/', login, name="login"),
     path('logout/', logout, name="logout"),
     path('addmessage/', add_message, name="add-message"),
-    path('<str:pk>/', user_timeline, name="user-timeline"),
+    path('<str:pk>/', user_profile_timeline, name="user-profile-timeline"),
     path('<str:pk>/follow', follow_user, name="follow-user"),
     path('<str:pk>/unfollow', unfollow_user, name="unfollow-user"),
 ]
