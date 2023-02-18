bdf17bf21190ce6b9c626d26fbc3e263ed804c87
PetraSt <petra.stamou@gmail.com>
2023-02-18 18:18:26 +0100

    add registration

--- ITU_MiniTwit/MiniTwit/urls.py
+++ ITU_MiniTwit/MiniTwit/urls.py
@@ -8,9 +8,10 @@ urlpatterns = [
     path('public/', public_timeline, name="public-timeline"),
     # path('login/', login, name="login"),
     # path('logout/', logout, name="logout"),
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path("accounts/", include("django.contrib.auth.urls"), name="accounts"),
     path('addmessage/', add_message, name="add-message"),
     path('<str:pk>/', user_profile_timeline, name="user-profile-timeline"),
     path('<str:pk>/follow', follow_user, name="follow-user"),
     path('<str:pk>/unfollow', unfollow_user, name="unfollow-user"),
-    path("accounts/", include("django.contrib.auth.urls"), name="accounts"),
 ]
--- ITU_MiniTwit/MiniTwit/views.py
+++ ITU_MiniTwit/MiniTwit/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.views import generic
+
+from .forms import CustomUserCreationForm
 from .models import Message, User, Follower
 
 def timeline(request):
@@ -72,3 +77,8 @@ def unfollow_user(request, pk):
 
 def add_message(request):
     return render(request, 'registration/login.html', {})
+
+class SignUpView(generic.CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("MiniTwit:login")
+    template_name = "registration/signup.html"
