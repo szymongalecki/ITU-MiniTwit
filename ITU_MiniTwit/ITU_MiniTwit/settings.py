1aa449e29c7f969453d8bbf5f134ef3dd9234c6b
Marcus Gunnebo <marcusgunnebo@Marcuss-MacBook-Air.local>
2023-02-21 16:04:25 +0100

    css for login and register

--- ITU_MiniTwit/ITU_MiniTwit/settings.py
+++ ITU_MiniTwit/ITU_MiniTwit/settings.py
@@ -119,8 +119,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [ os.path.join(BASE_DIR,'static') ]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # Default primary key field type
