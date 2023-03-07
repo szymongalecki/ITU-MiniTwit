d65bd6f4b5408d71c47d23e326df0c9c9d81c086
PetraSt <petra.stamou@gmail.com>
2023-03-07 22:33:19 +0100

    solve the password error

--- ITU_MiniTwit/MiniTwit/models.py
+++ ITU_MiniTwit/MiniTwit/models.py
@@ -1,16 +1,41 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser):
-    username = models.CharField(max_length=50, unique=True, null=False)
-    email = models.EmailField(unique=True, null=False)
 
 class Follower(models.Model):
-    who_id = models.ForeignKey(User, related_name='who_id', on_delete=models.CASCADE, null=True)
-    whom_id = models.ForeignKey(User, related_name='whom_id', on_delete=models.CASCADE, null=True)
+    who_id = models.IntegerField(blank=True, null=True)
+    whom_id = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'follower'
+
 
 class Message(models.Model):
-    author_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    text = models.CharField(max_length=280, null=False)
-    pub_date = models.DateTimeField(auto_now_add=True)
-    flagged = models.BooleanField(default=False)
+    message_id = models.AutoField(primary_key=True, blank=True, null=False)
+    author_id = models.IntegerField()
+    text = models.TextField()  # This field type is a guess.
+    pub_date = models.IntegerField(blank=True, null=True)
+    flagged = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'message'
+
+
+class User(AbstractUser):
+    user_id = models.AutoField(primary_key=True, blank=True, null=False)
+    username = models.TextField(max_length=50, unique=True, null=False)  # This field type is a guess.
+    email = models.TextField(unique=True, null=False)  # This field type is a guess.
+    password = models.CharField(db_column='pw_hash', max_length=128)   # This field type is a guess.
+
+    class Meta:
+        managed = False
+        db_table = 'user'
