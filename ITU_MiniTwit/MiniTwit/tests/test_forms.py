02f253e8e5b64492599fd53f7a72e25c98529157
Marcus Gunnebo <marcusgunnebo@Marcuss-MacBook-Air.local>
2023-03-25 13:51:29 +0100

    unit tests for APP

---
+++ ITU_MiniTwit/MiniTwit/tests/test_forms.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+from MiniTwit.forms import CustomUserCreationForm, CustomUserChangeForm
+
+class UserCreationFormTest(TestCase):
+
+    def test_form(self):
+        data = {
+            'username': 'testuser',
+            'email': 'testuser@gmail.com',
+            'password1': 'test123!',
+            'password2': 'test123!',
+        }
+
+        form = CustomUserCreationForm(data)
+
+        self.assertTrue(form.is_valid())
+
+class UserChangeFormTest(TestCase):
+
+    def test_form(self):
+        data = {
+            'username': 'testuser',
+            'email': 'testuser@gmail.com',
+            'password1': 'test123!',
+            'password2': 'test123!',
+        }
+
+        form = CustomUserChangeForm(data)
+
+        self.assertTrue(form.is_valid())
