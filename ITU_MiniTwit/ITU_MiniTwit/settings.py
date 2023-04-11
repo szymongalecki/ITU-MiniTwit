0d0ac998a59f0abf0ff6bc19528e8e6b74092438
dprzygocka <przygocka.dagmara@gmail.com>
2023-04-11 15:11:54 +0200

    missed file

--- ITU_MiniTwit/ITU_MiniTwit/settings.py
+++ ITU_MiniTwit/ITU_MiniTwit/settings.py
@@ -76,11 +76,6 @@ WSGI_APPLICATION = "ITU_MiniTwit.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-<<<<<<< HEAD
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-=======
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('POSTGRES_DB'),
@@ -92,7 +87,6 @@ DATABASES = {
     'old_db': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'minitwitOld.db',
->>>>>>> origin
     }
 }
 
