ece580925643597cd76cc50ad62d8064999e61ab
Marcus Gunnebo <marcusgunnebo@Marcuss-MacBook-Air.local>
2023-05-09 15:28:15 +0200

    test tweet

--- test.py
+++ test.py
@@ -5,6 +5,13 @@ import string
 import random
 import pytest
 
+GUI_URL = "http://138.68.73.127:8000/"
+
+def randomString(l):
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=l))
+
+test_tweet = randomString(5)
+
 def randomString(l):
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=l))
 
@@ -12,7 +19,7 @@ def setupDriver(subpage):
     options = Options()
     options.add_argument('--headless=new')
     driver = webdriver.Chrome(options=options)    
-    driver.get("http://138.68.73.127:8000/"+subpage+"/")
+    driver.get(GUI_URL+subpage+"/")
     return driver
 
 @pytest.fixture
@@ -42,13 +49,12 @@ def postTweet():
     driver.find_element("id", "id_password").send_keys("admin123!")
     driver.find_element("xpath", "/html/body/div/div[2]/form/button").click()
     time.sleep(2)
-    driver.find_element("id", "id_username").send_keys("testuser")
-    driver.find_element("xpath", "/html/body/div/div[2]/div[1]/form/p/input[1]").send_keys("this is a test tweet")
+    driver.find_element("xpath", "/html/body/div/div[2]/div[1]/form/p/input[1]").send_keys(test_tweet)
     driver.find_element("xpath", "/html/body/div/div[2]/div[1]/form/p/input[2]").click()
     time.sleep(2)
-    tweet = driver.find_element("xpath", "/html/body/div/div[2]/ul/li[2]/p")
-    return tweet
-
+    driver = setupDriver("76678")
+    tweet = driver.find_element("xpath", "/html/body/div/div[2]/ul/li[1]/p")
+    return tweet.text
 
 def test_signup(signUp):
     assert "Log In" in signUp
@@ -57,4 +63,4 @@ def test_login(logIn):
     assert "My Timeline" in logIn
 
 def test_post_tweet(postTweet):
-    assert "this is a test tweet" in postTweet
+    assert test_tweet in postTweet
