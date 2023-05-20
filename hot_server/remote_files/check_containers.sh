daaa14d68968a3e28556cda23e1e5a57a01d0ade
dprzygocka <przygocka.dagmara@gmail.com>
2023-05-20 14:45:56 +0200

    rolling updates

--- hot_server/remote_files/check_containers.sh
+++ hot_server/remote_files/check_containers.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-
-# Get the count of running Docker containers
 container_count=$(docker ps --format '{{.ID}}' | wc -l)
-
-# Define the threshold for container count
 threshold=10
-
 if [ "$container_count" -lt "$threshold" ]; then
     exit 1  # Return non-zero exit status to trigger switch
 else
