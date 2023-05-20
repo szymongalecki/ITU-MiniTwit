510ca4b6e4079a110d3108b6a9d3375acb4f713d
dprzygocka <przygocka.dagmara@gmail.com>
2023-05-20 12:14:15 +0200

    hot server implementation

---
+++ hot_server/remote_files/check_containers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get the count of running Docker containers
+container_count=$(docker ps --format '{{.ID}}' | wc -l)
+
+# Define the threshold for container count
+threshold=3
+
+if [ "$container_count" -lt "$threshold" ]; then
+    exit 1  # Return non-zero exit status to trigger switch
+else
+    exit 0  # Return zero exit status to indicate everything is fine
+fi
---
+++ hot_server/remote_files/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+. ~/.bash_profile
+
+cd /minitwit
+
+docker-compose -f docker-compose.yml pull
+docker-compose -f docker-compose.yml up -d
